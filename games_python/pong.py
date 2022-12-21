e3c3f21183a4ae1f4e6d6f73aa3b6ffaf71ab701
suryanshsinhsisodiya <suryanshsisodiya3106@gmail.com>
2022-12-21 21:15:58 +0530

    added ball

--- games_python/pong.py
+++ games_python/pong.py
@@ -9,20 +9,27 @@ def display():
     root.fill('#222222')
     pygame.draw.rect(root, '#ffffff', player1, border_radius=12)
     pygame.draw.rect(root, '#ffffff', player2, border_radius=12)
+    pygame.draw.rect(root, '#ffffff', ball, border_radius=10)
     pygame.display.update()
 
 
 def main():
-    global player1, player2
+    global player1, player2, ball
     FPS = 60
     clock = pygame.time.Clock()
     block_width = 12
     block_height = 125
+    ball_radius = 20
+    ball_velocity_x = 7
+    ball_velocity_y = 7
     player1 = pygame.Rect(10, 10, block_width, block_height)
     player2 = pygame.Rect(win_width - 22, 10, block_width, block_height)
+    ball = pygame.Rect(win_width / 2 - 10, win_height /
+                       2 - 10, ball_radius, ball_radius)
     while True:
         clock.tick(FPS)
-
+        ball.x += ball_velocity_x
+        ball.y += ball_velocity_y
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w] and player1.y > 10:
             player1.y -= 7
@@ -36,6 +43,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+
+        if ball.y <= 0 or ball.y >= win_height - ball.height:
+            ball_velocity_y *= -1
+        if ball.colliderect(player1) or ball.colliderect(player2):
+            ball_velocity_x *= -1
         display()
 
 
